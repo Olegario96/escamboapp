7fa6dc1d52fdcb3d0176428c9d48ebcca8f41e52
Olegario96 <gustavo-olegario@hotmail.com>
2018-01-24 23:20:21 +0000

    Fixing better_errors problem with Vagrant

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,8 +5,10 @@ Rails.application.configure do
   $stdout.sync = true
 
   # Better Errors gem Config
-  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
-  config.consider_all_requests_local = true
+  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
+
+  # WebConsole Rails
+  config.web_console.whitelisted_ips = ENV['TRUSTED_IP']
 
   # Devise gem config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
