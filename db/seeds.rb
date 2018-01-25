8b14eedd1c13c27bf417208cd730bd7570125931
Olegario96 <gustavo-olegario@hotmail.com>
2018-01-25 23:51:35 +0000

    Create new admins working

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,11 @@ categories.each do |category|
 end
 
 puts 'Categories created!'
+
+###############################################################################
+
+puts 'Creating the administrator...'
+
+Admin.create!(email: 'admin@admin.com', password: '123456', password_confirmation: '123456')
+
+puts 'Administrator created!'
