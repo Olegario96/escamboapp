e337820a11e47a0ccbf2d9ca625f3efb6f02e131
Olegario96 <gustavo-olegario@hotmail.com>
2018-01-25 15:32:31 +0000

    Edit and update for category working

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,6 @@
 class Backoffice::CategoriesController < BackofficeController
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end
@@ -20,9 +22,18 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def update
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "Categoria #{@category.description} atualizada!"
+    else
+      render :edit
+    end
   end
 
   private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
     def params_category
       params.require(:category).permit(:description)
     end
