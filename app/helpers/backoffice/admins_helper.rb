c00409d96fbc01d76b19db481ffb6d4a6f214684
Olegario96 <gustavo-olegario@hotmail.com>
2018-01-30 03:34:22 +0000

    Managin params using pundit

--- app/helpers/backoffice/admins_helper.rb
+++ app/helpers/backoffice/admins_helper.rb
@@ -1,2 +1,9 @@
 module Backoffice::AdminsHelper
+  RoleOption = Struct.new(:index, :description)
+
+  def options_for_role
+    Admin.roles_i18n.map do |key, value|
+      RoleOption.new(key, value)
+    end
+  end
 end
