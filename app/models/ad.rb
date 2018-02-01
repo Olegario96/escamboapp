81479a02bcde8937961458b646d4d665cd628c4e
Olegario96 <gustavo-olegario@hotmail.com>
2018-02-01 14:53:29 +0000

    Creating profile page for member

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -6,6 +6,6 @@ class Ad < ActiveRecord::Base
 
   monetize :price_cents
 
-  has_attached_file :picture, styles: { medium: '320x150#', thumb: '100x100>' }, default: '/images/:styles/missing.png'
+  has_attached_file :picture, styles: { medium: '320x150>', thumb: '100x100>' }, default: '/images/:styles/missing.png'
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
