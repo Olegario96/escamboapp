bec1f0e3d99df99499060f58c0c8425eb97ba8e0
Olegario96 <gustavo-olegario@hotmail.com>
2018-02-02 01:09:37 +0000

    Enabling edit for ads

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,5 +1,18 @@
 class Site::Profile::AdsController < Site::ProfileController
+  before_action :set_ad, only: [:edit]
+
   def index
     @ads = Ad.ads_for_current_member(current_member)
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+    def set_ad
+      @ad = Ad.find(params[:id])
+    end
 end
