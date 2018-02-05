56314bb609e47466ac8962e792e0306ed7afb15b
Olegario96 <gustavo-olegario@hotmail.com>
2018-02-05 22:07:37 +0000

    Creating view to show the complete ad

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -10,6 +10,6 @@ class Ad < ActiveRecord::Base
 
   monetize :price_cents
 
-  has_attached_file :picture, styles: { medium: '320x150>', thumb: '100x100>' }, default: '/images/:styles/missing.png'
+  has_attached_file :picture, styles: { medium: '320x150#', thumb: '100x100>', large: '800x300#' }, default: '/images/:styles/missing.png'
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
