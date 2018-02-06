7569d79145ff694a54ec99b412a8f25132ade1db
Olegario96 <gustavo-olegario@hotmail.com>
2018-02-06 00:50:36 +0000

    Creating new fields for ad and saving MD as HTML

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,8 +1,10 @@
 class Ad < ActiveRecord::Base
+  before_save :md_to_html
+
   belongs_to :member
   belongs_to :category
 
-  validates :title, :description, :category, :price, :picture, :finish_date, presence: true
+  validates :title, :description_md, :description_short, :category, :price, :picture, :finish_date, presence: true
   validates :price, numericality: { greater_than: 0 }
 
   scope :descending_order, ->(amount=9) { limit(amount).order(created_at: :desc) }
@@ -12,4 +14,25 @@ class Ad < ActiveRecord::Base
 
   has_attached_file :picture, styles: { medium: '320x150#', thumb: '100x100>', large: '800x300#' }, default: '/images/:styles/missing.png'
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
+  private
+    def md_to_html
+      options = {
+        filter_html: true,
+        link_attributes: {
+          rel: 'nofollow',
+          target: '_blank'
+        }
+      }
+
+      extensions = {
+        space_after_headers: true,
+        autolink: true
+      }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+      self.description = markdown.render(self.description_md)
+    end
 end
