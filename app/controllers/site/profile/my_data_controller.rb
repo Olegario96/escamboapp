6a0a5bfc2faaf0792e416a128f3369a41e6eaf92
Olegario96 <gustavo-olegario@hotmail.com>
2018-02-07 12:27:04 +0000

    Edit profile working

--- app/controllers/site/profile/my_data_controller.rb
+++ app/controllers/site/profile/my_data_controller.rb
@@ -1,5 +1,23 @@
 class Site::Profile::MyDataController < Site::ProfileController
+  before_action :set_profile_member, only: [:edit, :update]
+
   def edit
-    @profile_member = ProfileMember.find_or_create_by(member_id: current_member.id)
   end
+
+  def update
+    if @profile_member.update(params_profile_member)
+      redirect_to edit_site_profile_my_datum_path(current_member), notice: 'Perfil atualizado!'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def params_profile_member
+      params.require(:profile_member).permit(:id, :first_name, :second_name, :birthdate)
+    end
+
+    def set_profile_member
+      @profile_member = ProfileMember.find(current_member.id)
+    end
 end
