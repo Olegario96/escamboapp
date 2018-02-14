9450c738001ea8bcb3ea6806a052bdeb67e8f32f
Olegario96 <gustavo-olegario@hotmail.com>
2018-02-14 18:30:43 +0000

    Using nested validations to validates profile

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,10 +3,24 @@ class Member < ActiveRecord::Base
   has_one :profile_member
   accepts_nested_attributes_for :profile_member
 
+  validate :nested_attributes
+
   ratyrate_rater
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+    def nested_attributes
+      if nested_attributes_blank?
+        errors.add(:base, 'É necessário preencher o primeiro e segundo nome')
+      end
+    end
+
+    def nested_attributes_blank?
+      profile.member.first_name.blank? || profile.member.second_name.blank?
+    end
 end
