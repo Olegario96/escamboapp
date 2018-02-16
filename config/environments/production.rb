6621d7cc26957c97db1b952a71509325d2992a41
Olegario96 <gustavo-olegario@hotmail.com>
2018-02-16 11:47:22 +0000

    Partial config for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Devise config
+  config.action_mailer.default_url_options = { host: '159.65.161.128' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
