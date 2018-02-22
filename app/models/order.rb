2e38eaa11eafeb5bd71ca9ea4c6659a9bd89c293
Olegario96 <gustavo-olegario@hotmail.com>
2018-02-22 22:29:10 +0000

    Fixing error on Order for deploy

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :ad
-  belongs_to :buyer, class_name: 'Member'
+  belongs_to :buyer, class_name: 'Member', :foreign_key => 'buyer_id'
 
   enum status: [:requested, :waiting, :analysing, :paid, :available, :dispute, :returned, :canceled, :debited, :temporary_retention]
 end
